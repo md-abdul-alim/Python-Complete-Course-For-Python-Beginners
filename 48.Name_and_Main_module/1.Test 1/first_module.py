4a57ba087edcb7ee0237098513c33920abc433ed
Md. Abdul Alim <alim.abdul.5915@gmail.com>
2021-01-15 22:59:07 +0600

    Name and Main Module

---
+++ 48.Name_and_Main_module/1.Test 1/first_module.py
@@ -0,0 +1,10 @@
+
+
+print (__name__)
+print ("First Module's Name: {}".format(__name__))
+
+def main():
+	pass
+
+if __name__ == '__main__':
+	main()
---
+++ 48.Name_and_Main_module/2.Test 2/first_module.py
@@ -0,0 +1,10 @@
+
+
+
+
+
+def main():
+	print ("First Module's Name: {}".format(__name__))
+
+if __name__ == '__main__':
+	main()
